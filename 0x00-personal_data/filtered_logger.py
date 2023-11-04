ab12401f08c559396c9a5a152bb369b7fb338df1
Dominic <dominicudousoro@gmail.com>
2023-11-04 22:56:09 +0000

    Adding solutions to personal data tasks

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,8 +8,7 @@ import os
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-    """
+    """ Redacting Formatter class"""
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
